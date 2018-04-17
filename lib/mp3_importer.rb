f2df263d1444d41d07228a2634c22cd20c34b740
Karen G <kgraves0701@gmail.com>
2018-04-17 18:56:06 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,4 +6,11 @@ class MP3Importer
     @file = file
   end
 
+  def import 
+   imported_files = self.files
+   imported_files.each do |file|
+     new_song = Song.new_by_filename(file)
+   end
+ end
+
 end
