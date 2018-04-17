2aec7b2fe585faa9fe30fa9de0e8a6753548c46b
Karen G <kgraves0701@gmail.com>
2018-04-17 18:56:16 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,12 @@
 class MP3Importer
 
-  attr_accessor :file
+  # attr_accessor :file
+  #
+  # def initialize(file)
+  #   @file = file
+  # end
 
-  def initialize(file)
-    @file = file
-  end
-
-  def import 
+  def import
    imported_files = self.files
    imported_files.each do |file|
      new_song = Song.new_by_filename(file)
