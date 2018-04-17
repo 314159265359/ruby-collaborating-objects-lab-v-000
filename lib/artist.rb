d9bd58b2896e804126a05ce201e8f006237596df
Karen G <kgraves0701@gmail.com>
2018-04-17 19:22:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,34 +20,17 @@ class Artist
     end
 
     def self.find_or_create_by_name(name)
-    @@all.each do |artist|
-      if artist.name == name
-        return artist
+      @@all.each do |artist|
+        if artist.name == name
+        artist
+        # return artist
+        end
     end
-    end
-    
+
     artist = Artist.new(name)
     artist.save
     artist
   end
-
-    # def self.find_or_create_by_name(name)
-    #   if self.find(name)
-    #     self.find(name)
-    #   else
-    #     self.create(name)
-    #   end
-    # end
-    #
-    # def self.find(name)
-    #   self.all.detect { |artist| artist.name == name }
-    # end
-    #
-    # def self.create(name)
-    #   artist = Artist.new(name)
-    #   artist.save
-    #   artist
-    # end
 
     def print_songs
       self.songs.each do |song|
